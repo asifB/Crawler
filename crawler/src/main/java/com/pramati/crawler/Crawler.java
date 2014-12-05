267ae5990e356fa2727ae07f41da2990828ecf07
asifb <asifb@172.16.12.210>
2014-12-05 20:27:45 -0300

    Downloading mailboxes instead of files

--- crawler/src/main/java/com/pramati/crawler/Crawler.java
+++ crawler/src/main/java/com/pramati/crawler/Crawler.java
@@ -1,24 +1,34 @@
 package com.pramati.crawler;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/**
+ * 
+ * @author asifb
+ */
+
 public class Crawler {
 
 	public static List<String> visitedUrls = new ArrayList<String>();
 	public static List<String> unvisitedUrls = new ArrayList<String>();
-	public static final String baseUrl = "http://mail-archives.apache.org/mod_mbox/maven-users/";
+	public static final String BASEURL = "http://mail-archives.apache.org/mod_mbox/maven-users/";
 
-	public static void parseUrls(String url) {
+	/**
+	 * 
+	 * @param url
+	 */
+	public static void parseUrls(final String url) {
+
 		if (visitedUrls.contains(url)) {
 			return;
 		}
@@ -27,23 +37,17 @@ public class Crawler {
 		String absoluteURL = "";
 		try {
 			doc = Jsoup.connect(url).get();
-			Elements mailLinks = doc.select("a[href]");
-			for (Element link : mailLinks) {
-				absoluteURL = link.attr("abs:href");
+			final Elements mailLinks = doc.select("a[href]");
+			for (final Element link : mailLinks) {
+				absoluteURL = link.absUrl("href");
 				if (absoluteURL.contains("2014")
-						&& (absoluteURL.contains("date") || absoluteURL
-								.endsWith("%3e"))) {
+						&& absoluteURL.contains("date")) {
 					if (!visitedUrls.contains(absoluteURL)
 							|| !unvisitedUrls.contains(absoluteURL)) {
 						unvisitedUrls.add(absoluteURL);
-						if (!absoluteURL.endsWith("%3e")) {
-							parseUrls(absoluteURL);
-						}
+
 					}
 
-				}
-				if (!visitedUrls.contains(url)) {
-					visitedUrls.add(url);
 				}
 			}
 		} catch (IOException ie) {
@@ -51,65 +55,50 @@ public class Crawler {
 		}
 	}
 
-	
+	/**
+	 * @return void
+	 * @param url
+	 */
 	public static void downloadData(String url) {
-		try{
-/*		String url = "http://mail-archives.apache.org/mod_mbox/maven-users/201411.mbox/%3cCAKumxBwW6ArNEFSUUW=Q4bOvVXv=86GcB9JnX++JTz3Wbc04Bg@mail.gmail.com%3e";
-*/		Document doc = Jsoup.connect(url).get();
-		String fileName = "";
-		Elements elements = doc.select("td");
-		for(Element ele : elements){
-			if((ele.ownText()).equalsIgnoreCase("Subject")){
-				Element e = ele.nextElementSibling();
-				fileName = e.text();
-			}
-			if((ele.ownText()).equalsIgnoreCase("Date")){
-				Element e = ele.nextElementSibling();
-				fileName = fileName+"_"+e.text();
-			}
-			fileName = fileName.replaceAll("[:/\\*?\"<>,| ]", "_");
-		}
-		File parentDir = new File("./2014mails");
-		if(!parentDir.exists()){
-			boolean isDirectoryCreated = parentDir.mkdir();
-			if(!isDirectoryCreated){
-				return;
-			}
-		}
-		
-		String [] spliturl = url.split("/");
-		String monthName = spliturl[5].substring(0, 6);
-		
-		File monthlyDirName = new File(parentDir.getCanonicalPath()+"/"+monthName);
-		if(!monthlyDirName.exists()){
-			monthlyDirName.mkdir();
-		}
-		
-		File filePath = new File(monthlyDirName.getCanonicalPath()+"/"+fileName+".txt");
-		
-	/*	if(!filePath.exists()){
-			filePath.createNewFile();
-		}*/
-		
-		FileWriter fw = new FileWriter(filePath.getAbsoluteFile());
-		BufferedWriter bw = new BufferedWriter(fw);
-		bw.write(doc.text());
-		bw.close();
+		try {
+			final String fileName = getFileName(url);
+			URL finalUrl = new URL(url);
+			final File filePath = new File(fileName);
 
-		visitedUrls.add(url);
+			FileUtils.copyURLToFile(finalUrl, filePath);
 
-		}catch(IOException ie){
+			visitedUrls.add(url);
+
+		} catch (IOException ie) {
 			ie.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
+
+	private static String getFileName(String url) throws IOException {
+
+		final File parentDir = new File("./2014mails");
+		if (!parentDir.exists()) {
+			parentDir.mkdir();
+		}
+
+		final String[] spliturl = url.split("/");
+		final String fileName = spliturl[5];
+
+		String finalPath = parentDir + "/" + fileName;
+		return finalPath;
+	}
+
 	public static void main(String[] args) throws Exception {
-		parseUrls(baseUrl);
+		parseUrls(BASEURL);
 		for (String link : unvisitedUrls) {
-			if(!visitedUrls.contains(link)){
-			downloadData(link);
+			if (!visitedUrls.contains(link)) {
+				
+				downloadData(link.replace("/date", ""));
 			}
 		}
-		
+
 	}
+
 }
