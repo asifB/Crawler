caeaa5a77cee66272cacb30f4aabf3d6a78d6f80
asif.bhat <asif.bhat@imaginea.com>
2014-12-08 17:01:53 +0530

    Commiting

--- crawler/src/main/java/com/pramati/crawler/RunCrawler.java
+++ crawler/src/main/java/com/pramati/crawler/RunCrawler.java
@@ -4,10 +4,10 @@ public class RunCrawler {
 	
 	public static void main(String[] args) throws Exception {
 		Crawler crawler = new Crawler();
+		Downloader download = new Downloader();
 		crawler.parseUrls();
 		for (String link : crawler.unvisitedUrls) {
 			if (!crawler.visitedUrls.contains(link)) {
-				Downloader download = new Downloader();
 				download.downloadData(link.replace("/date", ""),crawler);
 			}
 		}
